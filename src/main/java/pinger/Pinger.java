3adb6aed204da4133b964869176a3ebe20bb1bf2
SD <harrypotter.starwars.shrish@gmail.com>
2020-09-30 22:19:09 +0530

    Initial commit
     Changes to be committed:
    	new file:   .gitattributes
    	new file:   .gitignore
    	new file:   build.gradle
    	new file:   gradle/wrapper/gradle-wrapper.jar
    	new file:   gradle/wrapper/gradle-wrapper.properties
    	new file:   gradlew
    	new file:   gradlew.bat
    	new file:   settings.gradle
    	new file:   src/main/java/pinger/Pinger.java

---
+++ src/main/java/pinger/Pinger.java
@@ -0,0 +1,35 @@
+package pinger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Pinger {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Enter the URL to be pinged");
+            String urlI = "http://" + reader.readLine();
+            URL url = new URL(urlI);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            try (InputStream stream = conn.getInputStream()) {
+                String content = new String(stream.readAllBytes());
+                if (content.isEmpty() && content.isBlank()) {
+                    content = "No content returned";
+                }
+                addField("Content Type", conn.getContentType());
+                addField("Request Method", conn.getRequestMethod());
+                addField("Connection Timeout", conn.getConnectTimeout());
+                addField("Response code", conn.getResponseCode());
+                addField("Header Fields", conn.getHeaderFields());
+                addField("Content", content);
+            }
+        }
+    }
+
+    private static void addField(String name, Object value) {
+        System.out.println(String.format(name + ": %s", value));
+    }
+}
